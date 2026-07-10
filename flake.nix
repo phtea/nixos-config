0f23ff9c72cc8d68a85a82a4b803c4fe3f6da394
phtea <phtea>
2026-07-10 23:11:16 +0300

    Added flake.nix

---
+++ flake.nix
@@ -0,0 +1,18 @@
+{
+	description = "Phtea's NixOS configuration";
+
+	inputs = {
+		nixpkgs.url = "github:NixOS/nixpkgs/nixos-26.05";
+	};
+
+	outputs = { self, nixpkgs }; {
+		nixosConfigurations.laptop =
+			nixpkgs.lib.nixosSystem {
+				system = "x86-64-linux";
+
+				modules = [
+					./hosts/laptop/default.nix
+				]
+			}
+	}
+}
