4e82356294ead7630293bd344342471183b7fc12
phtea <phtea>
2026-07-11 13:43:29 +0300

    nice touchpad and bgcolor

--- modules/home/sway.nix
+++ modules/home/sway.nix
@@ -1,25 +1,46 @@
 { ... }:
-
+ 
 {
-	wayland.windowManager.sway = {
-		enable = true;
-		wrapperFeatures.gtk = true;
-
-		config = {
-			terminal = "ghostty";
-			modifier = "Mod4";
-
-			input = {
-				"*" = {
-					xkb_layout = "us,ru";
-					xkb_options = "grp:win_space_toggle";
-				};
-			};
-
-			# TODO:
-			# bars = {
-			# 	position = "top";
-			# };
-		};
-	};
+  wayland.windowManager.sway = {
+    enable = true;
+ 
+    config = {
+      modifier = "Mod4";
+      terminal = "ghostty";
+ 
+      input = {
+        "type:keyboard" = {
+          xkb_layout = "us,ru";
+          xkb_options = "grp:win_space_toggle";
+        };
+ 
+        "type:touchpad" = {
+          tap = "enabled";
+          drag = "enabled";
+          dwt = "enabled";
+ 
+          natural_scroll = "enabled";
+          scroll_method = "two_finger";
+ 
+          click_method = "clickfinger";
+          tap_button_map = "lrm";
+ 
+          accel_profile = "adaptive";
+          pointer_accel = "0.25";
+        };
+      };
+ 
+      output = {
+        "*" = {
+          bg = "#1F1F1F solid_color";
+        };
+      };
+    };
+ 
+    extraConfig = ''
+      # Three-finger workspace navigation
+      bindgesture swipe:3:right workspace prev_on_output
+      bindgesture swipe:3:left workspace next_on_output
+    '';
+  };
 }
